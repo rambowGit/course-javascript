77f1752eeb2d513dd210e083247da82681490230
Stepkin Alexander <stepkin.alexander@gmail.com>
2021-11-01 18:47:18 +0300

    VP1 без карусели

--- projects/map/helper.js
+++ projects/map/helper.js
@@ -24,29 +24,35 @@ export const baloonTemplate = `
 </form>`;
 
 
+export const slideshowTemplateHeader = `
+<div class="container">
+    <div class='row'>
+        <div class='col-md-offset-2 col-md-8'>
+            <div class="carousel slide" data-ride="carousel" id="quote-carousel">
+                <div class="carousel-inner">
+                    <!-- Quote 1 -->
+                    <div class="item active">
+                        <blockquote>
+                            <div class="row">
+                                <div class="col-sm-9">
+                                    <p>`;
 
-/*
-этот шаблон используется для балуна при клике по метке
- */
-export const placemarkBaloonTemplate = `
+export const slideshowTemplateBody = `                                  
+                                     <li>Место: {{"mapLocation"}}</li>
+                                     <li>Отзыв: {{"userComment"}}</li>
+                                     <small>{{"userName"}}</small>
+                                    <!-- Carousel Buttons Next/Prev -->
+                                  <a data-slide="prev" href="#quote-carousel" class="left carousel-control"> <i class="fa fa-chevron-left"></i></a>
+                                  <a data-slide="next" href="#quote-carousel" class="right carousel-control"> <i class="fa fa-chevron-right"></i></a>`;
 
-      <form class = ".balloon_placemark_container">
-        <div class="comment_header">
-            Добавить отзыв:
+export const slideshowTemplateFooter = `
+                                    </p>
+                                </div>
+                            </div>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div >
-            <input  class="name" id="name" type = "text" name = "userName" placeholder="Укажите ваше имя"></div>
-        </div>
-        <div>
-                <input class = "location" id = "location" type = "text" name = "mapLocation" placeholder="Укажите место">
-        </div>
-        <div>
-                <input class = "comment_text" id = "comment_text" type = "text" name = "userComment" placeholder="Оставить отзыв">
-        </div>
-        <div>
-                <button class = "submit_btn" id = "submit_btn" type = "submit">Добавить</button>
-        </div>
-        
-      </form>    
-`;
-
+    </div>
+</div>`;
--- projects/map/index.js
+++ projects/map/index.js
@@ -2,25 +2,27 @@ import './index.html';
 import './css/normalize.css';
 import './css/style.css';
 import './helper';
-import {baloonTemplate, storage, placemarkBaloonTemplate} from "./helper";
+import {baloonTemplate, storage, slideshowTemplateHeader, slideshowTemplateBody, slideshowTemplateFooter} from "./helper";
 
 
 /*
  Получаем ранее оставленные placemarks из localstorage:
- const data = JSON.parse(localStorage.placemarkData); - общий скоуп данных
+ const data = JSON.parse(localStorage.mapData); - общий скоуп данных
  data[i] - данные конкретной точки, включая координаты и отзыв
  let formData = JSON.parse(data[i].balloonForm)
- или
- let formData = JSON.parse(data[i].balloonForm[j])
-
  */
 
 // данные
-const placemarksLocalStorage =  JSON.parse(localStorage.placemarkData || '[]');
+const pointStorage =  JSON.parse(localStorage.mapData || '[]');
 // массив объектов, в котором хранятся новые точки на карте
-let newPlacemarks = [];
-// форма отзыва их балуна
-let balloonForm;
+let newPoints = [];
+// глобальная переменная для хранения заполненной формы балуна и балуна метки
+let balloonForm = {};
+//
+let isNewBallon;
+let isPointBalloon;
+
+
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
@@ -56,19 +58,66 @@ window.addEventListener('DOMContentLoaded', (event) => {
        * и балуна геообъектов, можно посмотреть в документации.
        * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/GeoObject.xml
        */
+
       getPointData = function (index, data) {
 
        // предыдущие отзывы
         const pointBaloonTemplate = Handlebars.compile(
-          '{{#each balloonForm}}' +
-          '<li>Имя: {{"userName"}}</li>'+
+          '{{#each comments}}' +
+          '<li><b>Имя: {{"userName"}}</b></li>'+
           '<li>Место: {{"mapLocation"}}</li>'+
           '<li>Отзыв: {{"userComment"}}</li>'+
           '<hr>'+
           '{{/each}}'
         );
+
+
+         //TODO: доделать карусель
+
+        //  const pointBaloonTemplate = Handlebars.compile(
+        //    slideshowTemplateHeader+
+        //   '{{#each comments}}' +
+        //     slideshowTemplateBody +
+        //   '{{/each}}'+
+        //    slideshowTemplateFooter
+        // );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         return {
-          balloonContentBody:  placemarkBaloonTemplate,
+          balloonContentBody:  baloonTemplate,
           balloonContentFooter: '<span style="font-size: xx-small; ">Информация предоставлена: </span> балуном <strong>метки ' +index +'</strong><br>'+
                                 '<span style="font-size: xx-small; ">' +
                                  pointBaloonTemplate(data)
@@ -76,79 +125,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
         };
       },
 
-      myGeoObjects = [];
+      myPoints = [];
 
     /**
      * Данные передаются вторым параметром в конструктор метки, опции - третьим.
      * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Placemark.xml#constructor-summary
      */
 
-      // объект для хранения предыдущих отзывов
+    // устанавливаем из localsorage метки на карте
+    for(var i = 0, len = pointStorage.length; i < len; i++) {
 
-
-    for(var i = 0, len = placemarksLocalStorage.length; i < len; i++) {
-
-      let baloonData = {
-        balloonForm : []
+      let renderData = {
+        comments : []
       };
 
-
-      let point = placemarksLocalStorage[i];
-      console.log("point: ", point);
-
-      point['balloonForm'].forEach(arrayOfComments => baloonData.balloonForm.push(arrayOfComments));
-
-      console.log('baloonData of point: ', baloonData);
-
-
-     // устанавливаем из localsorage метки на карте
-      myGeoObjects[i] = new ymaps.Placemark(point['coords'], getPointData(i, baloonData));
-
-
-      // клики по метке - пока не используем
-      // myGeoObjects[i].events.add('click', function (e) {
-      //  // alert(formData);
-      // });
-
-      // сохранение формы балуна метки
-      myGeoObjects[i].balloon.events.add('open', function (e) {
-
-
-        /*
-          Сохранение данных балуна (отзыв) в localstorage.
-          Добавление метки.
-        */
-        document.addEventListener('submit', e =>{
-          //e.preventDefault();
-          let pointBalloonForm;
-
-          let userNameEl = document.querySelector('#name');
-          let mapLocationEl = document.querySelector('#location');
-          let userCommentEl = document.querySelector('#comment_text');
-
-          if(!userNameEl.value || !mapLocationEl.value || !userCommentEl.value)
-            alert('Пожалуйста, запоните все поля на форме.');
-
-          pointBalloonForm = {
-            userName: userNameEl.value,
-            mapLocation: mapLocationEl.value,
-            userComment: userCommentEl.value
-          };
-
-
-          if (pointBalloonForm !== undefined) {
-
-           // добавляем в localstorage новый отзыв
-           point['balloonForm'].push(pointBalloonForm);
-
-            // записываем массив текущих placemarks в localstorage
-            const concatPlacemarks = [...newPlacemarks, ...placemarksLocalStorage];
-            storage.placemarkData = JSON.stringify(concatPlacemarks);
-          }
+      pointStorage[i]['balloonForm']
+        .forEach(arrayOfComments => {
+          renderData.comments.push(arrayOfComments);
+          console.log(renderData);
 
         });
 
-      });
+      myPoints[i] = new ymaps.Placemark(pointStorage[i]['coords'], getPointData(i, renderData));
 
     }
 
@@ -156,21 +154,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
      * Можно менять опции кластеризатора после создания.
      */
     clusterer.options.set({
-      clusterDisableClickZoom: true
+      clusterDisableClickZoom: false
     });
 
     /**
      * В кластеризатор можно добавить javascript-массив меток (не геоколлекцию) или одну метку.
      * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Clusterer.xml#add
      */
-    clusterer.add(myGeoObjects);
+    clusterer.add(myPoints);
     myMap.geoObjects.add(clusterer);
 
     /**
      * Спозиционируем карту так, чтобы на ней были видны все объекты.
      */
 
-    if (myGeoObjects.length > 0) {
+    if (myPoints.length > 0) {
       myMap.setBounds(clusterer.getBounds(), {
         checkZoomRange: true
       });
@@ -178,73 +176,124 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 
 
-
-
-    // клики по карте
+    /*
+     клики по карте, создание балуна
+     */
+    let coords = [];
     myMap.events.add('click', function (event) {
 
-      // Получение координат щелчка
-      let coords = event.get('coords');
-      console.log('координаты клика:', coords);
+      isNewBallon = true;
 
-      // если балун закрыли, удаляем координаты, иначе появится placemark
-      // myMap.balloon.events.add('close', (e) => {
-      //   coords = [];
-      //   console.log("coords: ", coords);
-      //
-      // });
-      //
+      // Получение координат щелчка
+      coords = event.get('coords');
+      console.log('координаты клика:', coords);
 
       // открываем новый балун
       myMap.balloon.open(coords, {
         //здесь доработать код вывода формы ввода
-        contentBody: baloonTemplate
-      });
-
-      /*
-       Сохранение данных балуна (отзыв) в localstorage.
-       Добавление метки.
-      */
-      document.addEventListener('submit', e =>{
-       // e.preventDefault();
-
-        let userNameEl = document.querySelector('#name');
-        let mapLocationEl = document.querySelector('#location');
-        let userCommentEl = document.querySelector('#comment_text');
-
-        if(!userNameEl.value || !mapLocationEl.value || !userCommentEl.value)
-          alert('Пожалуйста, запоните все поля на форме.');
-
-
-        balloonForm = {
-          userName: userNameEl.value,
-          mapLocation: mapLocationEl.value,
-          userComment: userCommentEl.value
-        };
-
-
-        // добавляем новую метку
-        let newPlacemark = new ymaps.Placemark(coords, {
-
-        });
-
-        myMap.geoObjects.add(newPlacemark);
-        console.log('метка добавлена: ', event.get('coords'));
-
-        // тут храняться текущие плейсмарки, еще не сохраненные в local storage
-        newPlacemarks.push({
-          coords: coords,
-          balloonForm: [balloonForm]
-        });
-
-        // записываем массих текущих placemarks в localstorage
-        const concatPlacemarks = [...newPlacemarks, ...placemarksLocalStorage];
-        storage.placemarkData = JSON.stringify(concatPlacemarks);
-
-
+        contentBody: baloonTemplate,
       });
 
     });
+
+
+    /*
+     сохранение формы балуна метки
+     */
+    myPoints.forEach(point => {
+
+      point.balloon.events.add('open', function (e) {
+        // Получение координат метки
+        coords = point["geometry"]["_coordinates"];
+
+        isPointBalloon = true;
+
+      });
+    });
+
+
+
+    /*
+      Получение html-формы балуна
+    */
+      document.addEventListener('submit', (e) => {
+        // e.preventDefault();
+        if (e.target.classList.contains('balloon_container')) {
+          console.log ('сабмит балуна');
+          let userNameEl = document.querySelector('#name');
+          let mapLocationEl = document.querySelector('#location');
+          let userCommentEl = document.querySelector('#comment_text');
+
+          if(!userNameEl.value || !mapLocationEl.value || !userCommentEl.value)
+            alert('Пожалуйста, запоните все поля на форме.');
+
+          balloonForm = {
+            userName: userNameEl.value,
+            mapLocation: mapLocationEl.value,
+            userComment: userCommentEl.value
+          };
+
+          // проверяем, что это новый балун, пока без метки
+          if (isNewBallon){
+            saveNewPoint(balloonForm, coords);
+            isNewBallon = false;
+          }
+
+          // если обновляем балун метки
+          if (isPointBalloon){
+            updatePointBallon(balloonForm);
+            isPointBalloon = false;
+          }
+        }
+    });
+
+
+
+    /*
+    сохранение новой точки с новым балуном localstorage
+     */
+    function saveNewPoint(balloonForm, coords) {
+
+      // добавляем новую метку
+      let newPoint = new ymaps.Placemark(coords, {
+
+      });
+
+      myMap.geoObjects.add(newPoint);
+
+      // тут храняться текущие плейсмарки, еще не сохраненные в local storage
+      newPoints.push({
+        coords: coords,
+        balloonForm: [balloonForm]
+      });
+
+      // записываем массих текущих placemarks в localstorage
+      const concatPlacemarks = [...newPoints, ...pointStorage];
+      storage.mapData = JSON.stringify(concatPlacemarks);
+
+    }
+
+    /*
+    обновление балуна уже имеющейся точки
+     */
+    function updatePointBallon(balloonForm) {
+
+      pointStorage.forEach( point => {
+
+
+        if (point["coords"] === coords) {
+
+          console.log(point['coords']);
+
+          point['balloonForm'].push(balloonForm);
+          storage.mapData = JSON.stringify(pointStorage);
+        }
+
+
+
+      });
+
+    }
 
   });
 
